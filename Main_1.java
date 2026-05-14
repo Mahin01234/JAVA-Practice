104cf0b8749f67ee2e29675fee641a451bb01ed4
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-05-15 00:06:16 +0600

    Add files via upload

---
+++ Main_1.java
@@ -0,0 +1,14 @@
+
+
+
+public class Main_1 
+{
+  public static void main (String[] args) 
+  {
+    System.out.println ("id : 242014456") ; 
+    System.out.println ("title: iphone15") ; 
+    System.out.println ("price: 1895 euros") ;
+    System.out.println ("description: perfect product with best image quality") ;
+    System.out.println ("category: phone. ") ;
+  }
+}
