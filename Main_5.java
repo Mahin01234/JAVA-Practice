8d54028b028cbbb8f75ca3671500e91ff1f680d4
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-05-21 21:51:38 +0600

    Add files via upload

---
+++ Main_5.java
@@ -0,0 +1,11 @@
+public class Main_5
+{
+    public static void main (String[] args )
+    {
+        System.out.println("* * * * *") ;
+        System.out.println("* * * *") ;
+        System.out.println("* * *") ;
+        System.out.println("* *") ;
+        System.out.println("*") ;
+    }
+}
---
+++ Main_6.java
@@ -0,0 +1,11 @@
+public class Main_6
+{
+    public  static void main (String[] args )
+    {
+        System.out.println("        *") ;
+        System.out.println("      * *") ;
+        System.out.println("    * * *") ;
+        System.out.println("  * * * *") ;
+        System.out.println("* * * * *") ;
+    }
+}
