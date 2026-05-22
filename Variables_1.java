939a7a5beb1950618047f996e6f42d96fda71dba
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-05-22 10:14:23 +0600

    Add files via upload

---
+++ Variables_1.java
@@ -0,0 +1,30 @@
+public class Variables_1 
+{
+    
+    public static void main (String[] args ) 
+    {
+        String studentName = "John Doe";
+
+        int studentID = 15222;
+
+        int studentAge = 22;
+
+        float studentFee = 75.25f;
+
+        char studentGrade = 'B';
+
+
+        // Print variables
+
+        System.out.println("Student name: " + studentName);
+
+        System.out.println("Student id: " + studentID);
+
+        System.out.println("Student age: " + studentAge);
+
+        System.out.println("Student fee: " + studentFee);
+
+        System.out.println("Student grade: " + studentGrade);
+    }
+     
+}
