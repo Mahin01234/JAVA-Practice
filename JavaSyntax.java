6e0630f5bc5b973bfa8277c96869055a9c845012
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-02 00:33:41 +0600

    Add files via upload

---
+++ JavaSyntax.java
@@ -0,0 +1,20 @@
+public class JavaSyntax 
+{
+    public static void main (String[]  args ) 
+    {
+        System.out.println ("Hello , World ") ; 
+        System.out.println ("Welcome to JAVA Programming" )   ; 
+        System.out.println ("Java is fun!" )   ;    
+        System.out.println ("Have a good day!" )   ; 
+        System.out.println ("Learning Java is fun!" )   ; 
+        System.out.println ("Hello World!" )   ; 
+        System.out.println("This sentence will work!");
+        System.out.println("It is awesome!");
+        System.out.print("I will print on the same line.");
+
+
+
+
+    }
+    
+}
