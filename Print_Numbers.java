4a793d3cc4cca00fec17e700eea765bfaf490d76
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-02 00:50:50 +0600

    Add files via upload

---
+++ Print_Numbers.java
@@ -0,0 +1,18 @@
+public class Print_Numbers 
+{
+    public static void main (String[] args) 
+    {
+        System.out.println(3) ;
+        System.out.println(358) ;
+        System.out.println(50000) ; 
+        System.out.println(3 + 3) ; 
+
+        System.out.println(2 * 5);
+
+        System.out.println( 10 / 2) ; 
+
+        System.out.println(2.5) ; 
+
+    }
+    
+}
