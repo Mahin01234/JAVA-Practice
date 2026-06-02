2674610c54c23d74ac142e2fee8e6cb42b7538af
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-02 19:52:25 +0600

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,24 @@
+public class TypeCasting 
+{
+    
+
+
+    public static void main ( String[] args) 
+    {
+
+
+        int maxScore = 500;
+
+
+        // The actual score of the user
+
+        int userScore = 423;
+
+
+
+        double percentage = (double) userScore / maxScore * 100.0d;
+
+
+        System.out.println("User's percentage is " + percentage);
+    }
+}
