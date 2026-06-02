5f488398194abaf5cf11bc29eceb8f5a08d3333a
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-02 19:32:59 +0600

    Add files via upload

---
+++ Java_Variables.java
@@ -0,0 +1,27 @@
+public class Java_Variables
+
+{
+    public static void main (String[] args) 
+    {
+
+        int age = 23 ; 
+        String name = "Mahinur Rahman Mahin" ; 
+
+        char grade =  'A' ; 
+
+        boolean isStudent = true ; 
+
+
+        double cgpa = 3.79 ; 
+
+
+
+        System.out.println ("Name : " + name) ; 
+        System.out.println ("Age : " + age) ; 
+        System.out.println ("Grade : " + grade)  ; 
+        System.out.println ("CGPA : " + cgpa) ;  
+        System.out.println ("Is Student : " + isStudent)  ;  
+
+
+    }
+}
