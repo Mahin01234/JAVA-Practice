1a153e5fe25d648cb8c33edaec7f20dd469098fb
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-02 19:41:46 +0600

    Add files via upload

---
+++ TextNumbers.java
@@ -0,0 +1,20 @@
+public class TextNumbers 
+{
+
+    public static void main ( String[] args ) 
+    {
+
+
+        int x = 5;
+
+        int y = 6;
+
+
+        System.out.println("The sum is " + x + y);   
+
+        System.out.println("The sum is " + (x + y)) ; 
+
+        
+    } 
+    
+}
