3e30cfe320c3ca2ec844e3c8c04e0cd040b321f1
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:34:46 +0600

    Add files via upload

---
+++ compare_to.java
@@ -0,0 +1,23 @@
+public class compare_to
+{
+
+    public static void main (String[] args )
+    {
+
+        String N1 = "Mahi" , N2 = "Mahi" ;
+
+        System.out.println( N1.compareTo(N2) ) ;
+
+
+        String N3 = "Mahinur" , N4 = "Mahin" ;
+
+        System.out.println( N3.compareTo(N4) ) ;
+
+
+        String s1 = "Apple";
+        String s2 = "Banana";
+
+        System.out.println(s1.compareTo(s2));
+
+    }
+}
