412b925723c4e66eae547b864a111dd0867b97f5
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:22:01 +0600

    Add files via upload

---
+++ CharAt.java
@@ -0,0 +1,19 @@
+public class CharAt
+{
+
+    public static void main ( String[] args )
+    {
+
+        String  Name = "Mahinur" ;
+
+        System.out.println( Name.charAt(0) ) ;
+        System.out.println( Name.charAt(1) ) ;
+        System.out.println( Name.charAt(2) ) ;
+        System.out.println( Name.charAt(3) ) ;
+
+
+
+    }
+
+
+}
