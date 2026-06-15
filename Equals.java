c2edf8dd77261b1b58528555ccc2afb85b8675ad
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:22:21 +0600

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,18 @@
+public class Equals
+
+{
+
+    public static void main (String[] args )
+    {
+
+        Integer num1 = 7 , num2 = 99 ;
+
+
+        System.out.println( num1.equals(num2) ) ;
+
+
+        Integer num3 = 4 , num5 = 4 ;
+        System.out.println( num3.equals(num5) ) ;
+
+    }
+}
