ecb5c424234a9846c5c01892e262307c9e09ec35
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:35:38 +0600

    Add files via upload

---
+++ concat.java
@@ -0,0 +1,16 @@
+public class concat
+
+{
+
+    public static  void main (String[] args )
+    {
+
+        String first = "Java";
+        String second = " Programming";
+
+        System.out.println(first.concat(second));
+
+
+    }
+
+}
