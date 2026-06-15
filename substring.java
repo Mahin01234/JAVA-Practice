c18a8dd7f9436f1d970169f4246543d7603179e7
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:35:20 +0600

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,14 @@
+public class substring
+
+{
+
+    public static  void main (String[] args )
+    {
+
+        String name = "Mahin";
+
+        System.out.println(name.substring(1,4));
+
+
+    }
+}
