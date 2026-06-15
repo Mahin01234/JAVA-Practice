4888b7623a1bc1f285fc087f824aa281953d1aef
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2026-06-15 14:36:10 +0600

    Add files via upload

---
+++ toUpperCase.java
@@ -0,0 +1,15 @@
+public class toUpperCase
+
+{
+
+    public static void main (String[] args)
+    {
+
+        String name = "mahin";
+
+        System.out.println(name.toUpperCase());
+
+
+
+    }
+}
